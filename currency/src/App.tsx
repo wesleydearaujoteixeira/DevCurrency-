694091bd2abece9c1dcc4e79b7009a90f8e155c1
wesley <wesleyaraujoteixeira98@gmail.com>
2024-06-20 01:03:18 -0300

    first

---
+++ currency/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+export default function App() {
+  return (
+    <main className="bg-blue-600 text-white  text-7xl flex flex-col items-center justify-center w-newWidth h-newHeigth">
+
+      <h1> Olá Mundo </h1>
+
+    </main>
+  )
+}
